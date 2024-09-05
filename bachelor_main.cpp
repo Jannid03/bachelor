25aacc5cb1f08073ad9ff2093f3d20d23112fde4
Jannik Dubrau <jannid03@mi.fu-berlin.de>
2024-09-05 15:21:13 +0200

    Anpassung der Abarbeitung der Whrschpaare

--- bachelor_main.cpp
+++ bachelor_main.cpp
@@ -221,6 +221,10 @@ bool operator> (const prob & a, const prob & b) {
     return a.prio_ > b.prio_;
 }
 
+bool operator== (const prob & a, const prob & b) {
+    return ((a.x_ == b.x_) && (a.y_ == b.y_));
+}
+
 //Suchfunktion, fängt bei root an und sucht nach query. Tiefensuche
 //Returned pointer auf den node
 std::shared_ptr<node> find_node (const std::shared_ptr<node> & root, std::string query) {
@@ -577,7 +581,6 @@ std::pair<std::shared_ptr<node>, std::vector<std::shared_ptr<node>>> find_place(
                                                                     const std::shared_ptr<node> & root,
                                                                     const std::vector<prob> & probs,
                                                                     const size_t anfang,
-                                                                    const std::vector<prob> & ignored,
                                                                     std::vector<std::string> & in,
                                                                     const std::string & neu) {
 
@@ -713,61 +716,61 @@ std::pair<std::shared_ptr<node>, std::vector<std::shared_ptr<node>>> find_place(
                 }
             }
 
-            for (size_t i = 0; i < ignored.size(); i++) {
-                if((ignored[i].x_ == neu) && (finden(visited, ignored[i].y_))) {
-                    value += ignored[i].probs_[1];  
-                    if (ignored[i].max_ != 1 && ignored[i].max_ != 10 && ignored[i].max_ != 21) {
-                        local_con = true;
-                        local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
-                        local_eigent = ignored[i].max_;
-                        local_statt = 1;
-                        fall = "EI1";
-                    }  
-                }
-                else if ((ignored[i].y_ == neu) && (finden(visited, ignored[i].x_))) {
-                    value += ignored[i].probs_[0];
-                    if (ignored[i].max_ != 0 && ignored[i].max_ != 10 && ignored[i].max_ != 20) {
-                        local_con = true;
-                        local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
-                        local_eigent = ignored[i].max_;
-                        local_statt = 0;
-                        fall = "EI2";
-                    }
-                }
-                else if((ignored[i].x_ == neu) && (std::find(nach.begin(), nach.end(), ignored[i].y_) != nach.end())) {
-                    value += ignored[i].probs_[0];  
-                    if (ignored[i].max_ != 0 && ignored[i].max_ != 10 && ignored[i].max_ != 20) {
-                        local_con = true;
-                        local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
-                        local_eigent = ignored[i].max_;
-                        local_statt = 0;
-                        fall = "EI3";
-                    }  
-                }
-                else if ((ignored[i].y_ == neu) && (std::find(nach.begin(), nach.end(), ignored[i].x_) != nach.end())) {
-                    value += ignored[i].probs_[1];
-                    if (ignored[i].max_ != 1 && ignored[i].max_ != 10 && ignored[i].max_ != 21) {
-                        local_con = true;
-                        local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
-                        local_eigent = ignored[i].max_;
-                        local_statt = 1;
-                        fall = "EI4";
-                    }
-                }
-                else if (((ignored[i].x_ == neu) && (std::find(in.begin(), in.end(), ignored[i].y_) != in.end())) || ((ignored[i].y_ == neu) && (std::find(in.begin(), in.end(), ignored[i].x_) != in.end()))) {
-                    value += ignored[i].probs_[2];
-                    if (ignored[i].max_ != 2 && ignored[i].max_ != 20 && ignored[i].max_ != 21) {
-                        local_con = true;
-                        local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
-                        local_eigent = ignored[i].max_;
-                        local_statt = 2;
-                        fall = "EI5";
-                    }
-                }
-                else {
-                    continue;
-                }
-            }
+            // for (size_t i = 0; i < ignored.size(); i++) {
+            //     if((ignored[i].x_ == neu) && (finden(visited, ignored[i].y_))) {
+            //         value += ignored[i].probs_[1];  
+            //         if (ignored[i].max_ != 1 && ignored[i].max_ != 10 && ignored[i].max_ != 21) {
+            //             local_con = true;
+            //             local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
+            //             local_eigent = ignored[i].max_;
+            //             local_statt = 1;
+            //             fall = "EI1";
+            //         }  
+            //     }
+            //     else if ((ignored[i].y_ == neu) && (finden(visited, ignored[i].x_))) {
+            //         value += ignored[i].probs_[0];
+            //         if (ignored[i].max_ != 0 && ignored[i].max_ != 10 && ignored[i].max_ != 20) {
+            //             local_con = true;
+            //             local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
+            //             local_eigent = ignored[i].max_;
+            //             local_statt = 0;
+            //             fall = "EI2";
+            //         }
+            //     }
+            //     else if((ignored[i].x_ == neu) && (std::find(nach.begin(), nach.end(), ignored[i].y_) != nach.end())) {
+            //         value += ignored[i].probs_[0];  
+            //         if (ignored[i].max_ != 0 && ignored[i].max_ != 10 && ignored[i].max_ != 20) {
+            //             local_con = true;
+            //             local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
+            //             local_eigent = ignored[i].max_;
+            //             local_statt = 0;
+            //             fall = "EI3";
+            //         }  
+            //     }
+            //     else if ((ignored[i].y_ == neu) && (std::find(nach.begin(), nach.end(), ignored[i].x_) != nach.end())) {
+            //         value += ignored[i].probs_[1];
+            //         if (ignored[i].max_ != 1 && ignored[i].max_ != 10 && ignored[i].max_ != 21) {
+            //             local_con = true;
+            //             local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
+            //             local_eigent = ignored[i].max_;
+            //             local_statt = 1;
+            //             fall = "EI4";
+            //         }
+            //     }
+            //     else if (((ignored[i].x_ == neu) && (std::find(in.begin(), in.end(), ignored[i].y_) != in.end())) || ((ignored[i].y_ == neu) && (std::find(in.begin(), in.end(), ignored[i].x_) != in.end()))) {
+            //         value += ignored[i].probs_[2];
+            //         if (ignored[i].max_ != 2 && ignored[i].max_ != 20 && ignored[i].max_ != 21) {
+            //             local_con = true;
+            //             local_conflicts = std::pair<std::string, std::string> {ignored[i].x_, ignored[i].y_};
+            //             local_eigent = ignored[i].max_;
+            //             local_statt = 2;
+            //             fall = "EI5";
+            //         }
+            //     }
+            //     else {
+            //         continue;
+            //     }
+            // }
 
             // if(anfang == 35) {std::cout << "Value before new Kante: " << value << std::endl;}
 
@@ -844,7 +847,7 @@ std::pair<std::shared_ptr<node>, std::vector<std::shared_ptr<node>>> find_place(
 }
 
 // [Rcpp::export]
-void make_tree (const std::vector<prob> & probs, double fn, double fp, int mut, int cells, std::string datamat) {
+void make_tree (std::vector<prob> & probs, double fn, double fp, int mut, int cells, const std::string datamat) {
     std::shared_ptr<node> root (new normal_node (std::string ("root")));
 
     //Initialiserung mit erstem Paar
@@ -865,7 +868,7 @@ void make_tree (const std::vector<prob> & probs, double fn, double fp, int mut, 
     }
 
     std::vector<std::string> in {probs[0].x_, probs[0].y_};
-    std::vector<prob> ignored;
+    std::vector<prob> used;
     std::vector<conflict> conflicts;
 
 
@@ -873,30 +876,92 @@ void make_tree (const std::vector<prob> & probs, double fn, double fp, int mut, 
     //Wenn beide ja -> continue
     //Wenn nur eins ja -> wird an find_place übergeben und in den Tree eingefügt
     //Wenn kein ja -> continue und Wahrscheinlichkeit wird in ginored eingefügt
-    for (size_t i = 1; i < probs.size(); i++) {
-        // std::cout << "Test, i: " << i << std::endl;
-        // if ( i == 35) {
-        //     tree_ausgabe(root, "test");
-        // }
+
+    //ALT!!!!!!
+
+    // for (size_t i = 1; i < probs.size(); i++) {
+    //     // std::cout << "Test, i: " << i << std::endl;
+    //     // if ( i == 35) {
+    //     //     tree_ausgabe(root, "test");
+    //     // }
+    //     std::pair<std::shared_ptr<node>, std::vector<std::shared_ptr<node>>> place;
+
+    //     if ((std::find(in.begin(), in.end(), probs[i].x_) != in.end()) && (std::find(in.begin(), in.end(), probs[i].y_) != in.end())) {
+    //         //std::cout << "Fall 1" << std::endl;
+    //         continue;
+    //     }
+    //     else if((std::find(in.begin(), in.end(), probs[i].x_) != in.end())) {
+    //         // std::cout << "Fall 2" << std::endl;
+    //         place = find_place(conflicts, root, probs, i, ignored, in, probs[i].y_);
+    //         in.push_back(probs[i].y_);
+    //     }
+    //     else if ((std::find(in.begin(), in.end(), probs[i].y_) != in.end())) {
+    //         // std::cout << "Fall 3" << std::endl;
+    //         place = find_place(conflicts, root, probs, i, ignored, in, probs[i].x_);
+    //         in.push_back(probs[i].x_);
+    //     }
+    //     else {
+    //         //std::cout << "Fall 4" << std::endl;
+    //         ignored.push_back(probs[i]);
+    //         continue;
+    //     }
+        
+    //     //Neuer node
+    //     std::shared_ptr<node> new_node (new normal_node (in[in.size()-1]));
+
+    //     //Wenn place.second nicht "leer" -> Kinder werden hinzugefügt und gelöscht aus vormaligen Elternknoten
+    //     if (place.second[0] != nullptr) {
+    //         // std::cout << "Kante mit: " << std::endl;
+    //         for (size_t i = 0; i < place.second.size(); i++) { 
+    //             // std::cout << place.second[i] -> label_ << ", ";
+    //             new_node -> children_.push_back(place.second[i]);
+    //             place.first -> children_.erase(std::find(place.first -> children_.begin(), place.first -> children_.end(), place.second[i]));
+    //         }
+    //         place.first -> children_.push_back(new_node);
+
+            
+    //     }
+    //     else { //Ansonsten neuer Knoten
+    //         // std::cout << "i: " << i << " mit else" << std::endl;
+    //         // std::cout << "First: " << (place.first == nullptr) << " und second: " << place.second.size() << std::endl;
+    //         // std::cout << (place.first == nullptr) << std::endl;
+    //         place.first -> children_.push_back(new_node);
+    //         // std::cout << "Test in else" << std::endl;
+    //     }
+    //     // std::cout << "Test nach einfugen" << std::endl;
+    //     // tree_ausgabe(root);
+    // }
+
+    int i {0};
+    while(i < probs.size()) {
         std::pair<std::shared_ptr<node>, std::vector<std::shared_ptr<node>>> place;
 
         if ((std::find(in.begin(), in.end(), probs[i].x_) != in.end()) && (std::find(in.begin(), in.end(), probs[i].y_) != in.end())) {
             //std::cout << "Fall 1" << std::endl;
+            used.push_back(probs[i]);
+            probs.erase(std::find(probs.begin(), probs.end(), probs[i]));
+            i = 0;
             continue;
         }
         else if((std::find(in.begin(), in.end(), probs[i].x_) != in.end())) {
             // std::cout << "Fall 2" << std::endl;
-            place = find_place(conflicts, root, probs, i, ignored, in, probs[i].y_);
+            place = find_place(conflicts, root, probs, i, in, probs[i].y_);
             in.push_back(probs[i].y_);
+            used.push_back(probs[i]);
+            probs.erase(std::find(probs.begin(), probs.end(), probs[i]));
+            i = 0;
         }
         else if ((std::find(in.begin(), in.end(), probs[i].y_) != in.end())) {
             // std::cout << "Fall 3" << std::endl;
-            place = find_place(conflicts, root, probs, i, ignored, in, probs[i].x_);
+            place = find_place(conflicts, root, probs, i, in, probs[i].x_);
             in.push_back(probs[i].x_);
+            used.push_back(probs[i]);
+            probs.erase(std::find(probs.begin(), probs.end(), probs[i]));
+            i = 0;
         }
         else {
             //std::cout << "Fall 4" << std::endl;
-            ignored.push_back(probs[i]);
+            i++;
             continue;
         }
         
@@ -925,6 +990,8 @@ void make_tree (const std::vector<prob> & probs, double fn, double fp, int mut, 
         // std::cout << "Test nach einfugen" << std::endl;
         // tree_ausgabe(root);
     }
+
+
     std::cout << "Konflikt size: " << conflicts.size() << std::endl;
     tree_ausgabe(root, "graph_vorher");
     int* parent_vec_vorher = to_parent_vec(root);
